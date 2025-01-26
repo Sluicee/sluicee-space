eba02a997ed7917fee612d4de8fe9ace0c803cab
Sluicee <sluicee1@gmail.com>
2025-01-26 14:47:02 +0400

    settings

--- sluiceeSpace/settings.py
+++ sluiceeSpace/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sluicee.space', 'www.sluicee.space', '127.0.0.1', '192.168.0.11']
+ALLOWED_HOSTS = ['sluicee.ru', 'www.sluicee.ru', 'sluicee.space', 'www.sluicee.space', '127.0.0.1', '192.168.0.11']
 
 
 # Application definition
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'sluiceeSpace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
     }
 }
 
