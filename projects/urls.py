369fb3940a74bcb059402e573b0aa074a1357295
Sluicee <sluicee1@gmail.com>
2025-01-29 19:24:06 +0400

    error pages

--- projects/urls.py
+++ projects/urls.py
@@ -6,3 +6,8 @@ urlpatterns = [
     path('projects/', views.project_list, name='project_list'),
     path('projects/<slug:slug>/', views.project_detail, name='project_detail'),
 ]
+
+handler400 = 'projects.views.bad_request'
+handler403 = 'projects.views.permission_denied'
+handler404 = 'projects.views.page_not_found'
+handler500 = 'projects.views.server_error'
--- projects/views.py
+++ projects/views.py
@@ -18,3 +18,15 @@ def index(request):
         'projects': projects
     }
     return render(request, 'index.html', context)
+
+def page_not_found(request, exception):
+    return render(request, '404.html', status=404)
+
+def server_error(request):
+    return render(request, '500.html', status=500)
+
+def permission_denied(request, exception):
+    return render(request, '403.html', status=403)
+
+def bad_request(request, exception):
+    return render(request, '400.html', status=400)
